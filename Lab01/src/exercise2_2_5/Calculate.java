163e4f7accf9fb51b19c4e78b5c4f1d006c16fbf
Nguyen Phu Lam <phulamcllc@gmail.com>
2026-03-24 21:57:31 +0700

    2.2.6

--- Lab01/src/exercise2_2_5/Calculate.java
+++ Lab01/src/exercise2_2_5/Calculate.java
@@ -6,6 +6,12 @@ public class Calculate {
 		String snum1;
 		String snum2;
 		String command = JOptionPane.showInputDialog(null, "What do you want to do? \n Type 1 to calculate sum \n Type 2 to calculate difference \n Type 3 to calculate product \n Type 4 to calculate quotient");
+		double icom=Double.parseDouble(command);
+		if(icom !=1 && icom!=2 && icom!=3) 
+		{
+			JOptionPane.showMessageDialog(null, "Error: Invalid Selection");
+			System.exit(0);
+		}
 		snum1 = JOptionPane.showInputDialog(null, "Enter the first number:", "First number", JOptionPane.INFORMATION_MESSAGE);
 		snum2 = JOptionPane.showInputDialog(null, "Enter the second number:", "Second number", JOptionPane.INFORMATION_MESSAGE);
 		double num1 = Double.parseDouble(snum1);
@@ -13,8 +19,7 @@ public class Calculate {
 		double sum = num1+num2;
 		double diff= num1-num2;
 		double prod= num1*num2;
-		
-		double icom=Double.parseDouble(command);
+	
 		if(icom == 1)
 		{
 			JOptionPane.showMessageDialog(null, sum);

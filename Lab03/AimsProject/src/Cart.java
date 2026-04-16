8b5953617c7ab40f2b5e49d27b07b2e4c0a892f0
Nguyen Phu Lam <phulamcllc@gmail.com>
2026-04-16 20:38:16 +0700

    Method overloading in Cart

--- Lab03/AimsProject/src/Cart.java
+++ Lab03/AimsProject/src/Cart.java
@@ -1,44 +1,73 @@
+package hust.soict.dsai.aims.cart;
+
+import hust.soict.dsai.aims.disc.DigitalVideoDisc;
+
 public class Cart {
     public static final int MAX_NUMBERS_ORDERED = 20;
     private DigitalVideoDisc itemsOrdered[] = new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
     private int qtyOrdered = 0;
 
+    //Thêm 1 đĩa (Phương thức gốc)
     public void addDigitalVideoDisc(DigitalVideoDisc disc) {
         if (qtyOrdered < MAX_NUMBERS_ORDERED) {
             itemsOrdered[qtyOrdered] = disc;
             qtyOrdered++;
-            System.out.println("The disc has been added.");
+            System.out.println("The disc \"" + disc.getTitle() + "\" has been added.");
         } else {
-            System.out.println("The cart is almost full.");
+            System.out.println("The cart is almost full. Cannot add: " + disc.getTitle());
         }
+    }
+
+    // Nạp chồng Thêm một danh sách đĩa (Mảng)
+    public void addDigitalVideoDisc(DigitalVideoDisc[] dvdList) {
+        for (DigitalVideoDisc disc : dvdList) {
+            addDigitalVideoDisc(disc);
+        }
+    }
+
+    //Nạp chồng Thêm số lượng tham số tùy ý (Varargs)
+    public void addDigitalVideoDisc(DigitalVideoDisc... dvds) {
+        for (DigitalVideoDisc disc : dvds) {
+            addDigitalVideoDisc(disc);
+        }
+    }
+
+    //Nạp chồng Thêm đúng 2 đĩa cùng lúc
+    public void addDigitalVideoDisc(DigitalVideoDisc dvd1, DigitalVideoDisc dvd2) {
+        addDigitalVideoDisc(dvd1);
+        addDigitalVideoDisc(dvd2);
     }
 
     public void removeDigitalVideoDisc(DigitalVideoDisc disc) {
         int indexFound = -1;
         
-        //Tìm vị trícủa đĩa cần xóa
+        // Tìm vị trí của đĩa cần xóa
         for (int i = 0; i < qtyOrdered; i++) {
             if (itemsOrdered[i] == disc) {
+                indexFound = i;
                 break;
             }
         }
-        //xóa & dồn mảng
+        
+        // Xóa và dồn mảng
         if (indexFound != -1) {
             for (int i = indexFound; i < qtyOrdered - 1; i++) {
                 itemsOrdered[i] = itemsOrdered[i + 1]; 
             }
             itemsOrdered[qtyOrdered - 1] = null; 
             qtyOrdered--;
-            System.out.println("The disc has been removed.");
+            System.out.println("The disc \"" + disc.getTitle() + "\" has been removed.");
         } else {
-            System.out.println("The disc was not found in the cart.");
+            System.out.println("The disc \"" + disc.getTitle() + "\" was not found in the cart.");
         }
     }
 
     public float totalCost() {
         float total = 0;
         for (int i = 0; i < qtyOrdered; i++) {
-            total += itemsOrdered[i].getCost();
+            if (itemsOrdered[i] != null) {
+                total += itemsOrdered[i].getCost();
+            }
         }
         return total;
     }
